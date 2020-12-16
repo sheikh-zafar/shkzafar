2c722a4b3f0434f2b6166229b2a5a208f98f4568
ithaksinterlance <it.haksinterlance@gmail.com>
2020-12-16 22:32:41 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Hyundai from "./img/hyundai.png";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Dars of Sheikh zafar-ul-Hassan (hafizahullah)</h1>
+      <img src={Hyundai} alt="sample-img"/>
+    </div>
+  );
+}
+
+export default App;
