bfb6928fd6712dca52cb921bdce81ea9755a3581
ithaksinterlance <it.haksinterlance@gmail.com>
2020-12-20 22:27:52 +0530

    message

--- src/components/audio.js
+++ src/components/audio.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Navbar from "./Navbar";
 import Footer from "./footer";
-import pics from "../img/bam2.jpg";
 
 export default class audio extends Component {
   render() {
@@ -24,19 +23,37 @@ export default class audio extends Component {
             <div className="row text-center">
               <div className="col-md-4 d-flex justify-content-start d-md-flex justify-content-center">
                 <a href="/" rel="noopener noreferrer">
-                  <div className="rounded-links rounded-circle"></div>
+                  <div className="rounded-links rounded-circle">
+                    <i
+                      className="fa fa-5x fa-calendar pt-4"
+                      style={{ color: "#2e856e" }}
+                      aria-hidden="true"
+                    ></i>
+                  </div>
                   <h3 className="rounded-headname">خطبة</h3>
                 </a>
               </div>
               <div className="col-md-4 d-flex justify-content-center d-md-flex justify-content-center">
                 <a href="/" rel="noopener noreferrer">
-                  <div className="rounded-links rounded-circle"></div>
-                  <h3 className="rounded-headname">خطبة</h3>
+                  <div className="rounded-links rounded-circle">
+                    <i
+                      className="fa fa-5x fa-calendar pt-4"
+                      style={{ color: "#2e856e" }}
+                      aria-hidden="true"
+                    ></i>
+                  </div>
+                  <h3 className="rounded-headname">تدوين السنة</h3>
                 </a>
               </div>
               <div className="col-md-4 d-flex justify-content-end d-md-flex justify-content-center">
                 <a href="/" rel="noopener noreferrer">
-                  <div className="rounded-links rounded-circle"></div>
+                  <div className="rounded-links rounded-circle">
+                    <i
+                      className="fa fa-5x fa-calendar pt-4"
+                      style={{ color: "#2e856e" }}
+                      aria-hidden="true"
+                    ></i>
+                  </div>
                   <h3 className="rounded-headname">خطبة</h3>
                 </a>
               </div>
@@ -49,20 +66,42 @@ export default class audio extends Component {
             <div className="row text-center d-flex justify-content-center">
               <div className="col-md-5 d-flex justify-content-end d-md-flex justify-content-center">
                 <a href="/" rel="noopener noreferrer">
-                  <div className="rounded-links rounded-circle"></div>
+                  <div className="rounded-links rounded-circle">
+                    <i
+                      className="fa fa-5x fa-calendar pt-4"
+                      style={{ color: "#2e856e" }}
+                      aria-hidden="true"
+                    ></i>
+                  </div>
                   <h3 className="rounded-headname">خطبة</h3>
                 </a>
               </div>
               <div className="col-md-5 d-flex justify-content-start d-md-flex justify-content-center">
                 <a href="/" rel="noopener noreferrer">
-                  <img
-                    className="rounded-links rounded-circle"
-                    src={pics}
-                    alt="pics"
-                    rel="noopener noreferrer"
-                  />
+                  <div className="rounded-links rounded-circle">
+                    <i
+                      className="fa fa-5x fa-calendar pt-4"
+                      style={{ color: "#2e856e" }}
+                      aria-hidden="true"
+                    ></i>
+                  </div>
                   <h3 className="rounded-headname">خطبة</h3>
                 </a>
+              </div>
+            </div>
+          </div>
+        </section>
+        {/*audio cards */}
+        <section className="embed-section">
+          <div className="container">
+            <div className="row text-center">
+              <div class="col-md-12">
+                <iframe
+                  title="shaikh zafar class"
+                  src="https://drive.google.com/embeddedfolderview?id=1F7mRlSHiaPJH0ugBAdYLIVc5vD7FyVLh#grid"
+                  height="270px"
+                  frameborder="0"
+                ></iframe>
               </div>
             </div>
           </div>
