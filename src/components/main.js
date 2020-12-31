1d0731429e30691bd22fcb315ee287c61b8fa7e0
ithaksinterlance <it.haksinterlance@gmail.com>
2020-12-31 11:57:53 +0530

    message

--- src/components/main.js
+++ src/components/main.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Router } from "react-router-dom";
 import Home from "./Home";
 import audio from "./audio";
 import books from "./books";
@@ -12,24 +12,18 @@ import arkan from "./arkan";
 import taweel from "./taweel";
 
 const Main = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/shkzafar" component={Home} />
-      <Route exact path="/shkzafar/audio" component={audio} />
-      <Route
-        exact
-        path="/shkzafar/audio/qurantafseer"
-        component={qurantafseer}
-      />
-      <Route exact path="/shkzafar/audio/aam" component={aam} />
-      <Route exact path="/shkzafar/audio/arkan" component={arkan} />
-      <Route exact path="/shkzafar/audio/taweel" component={taweel} />
-      <Route exact path="/shkzafar/audio/muktasar" component={muktasar} />
-      <Route exact path="/shkzafar/audio/khutbah" component={khutbah} />
-      <Route exact path="/shkzafar/books" component={books} />
-      <Route exact path="/shkzafar/about" component={about} />
-    </Switch>
-  </BrowserRouter>
+  <Router>
+    <Route exact path="/shkzafar" component={Home} />
+    <Route exact path="/shkzafar/audio" component={audio} />
+    <Route exact path="/shkzafar/audio/qurantafseer" component={qurantafseer} />
+    <Route exact path="/shkzafar/audio/aam" component={aam} />
+    <Route exact path="/shkzafar/audio/arkan" component={arkan} />
+    <Route exact path="/shkzafar/audio/taweel" component={taweel} />
+    <Route exact path="/shkzafar/audio/muktasar" component={muktasar} />
+    <Route exact path="/shkzafar/audio/khutbah" component={khutbah} />
+    <Route exact path="/shkzafar/books" component={books} />
+    <Route exact path="/shkzafar/about" component={about} />
+  </Router>
 );
 
 export default Main;
