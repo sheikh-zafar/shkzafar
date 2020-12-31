845ec1fde021bf9cd7737f392ced5ab5d4dde00c
ithaksinterlance <it.haksinterlance@gmail.com>
2020-12-31 12:09:33 +0530

    message

--- src/components/main.js
+++ src/components/main.js
@@ -12,18 +12,24 @@ import arkan from "./arkan";
 import taweel from "./taweel";
 
 const Main = () => (
-  <Router>
-    <Route exact path="/shkzafar" component={Home} />
-    <Route exact path="/shkzafar/audio" component={audio} />
-    <Route exact path="/shkzafar/audio/qurantafseer" component={qurantafseer} />
-    <Route exact path="/shkzafar/audio/aam" component={aam} />
-    <Route exact path="/shkzafar/audio/arkan" component={arkan} />
-    <Route exact path="/shkzafar/audio/taweel" component={taweel} />
-    <Route exact path="/shkzafar/audio/muktasar" component={muktasar} />
-    <Route exact path="/shkzafar/audio/khutbah" component={khutbah} />
-    <Route exact path="/shkzafar/books" component={books} />
-    <Route exact path="/shkzafar/about" component={about} />
-  </Router>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/shkzafar" component={Home} />
+      <Route exact path="/shkzafar/audio" component={audio} />
+      <Route
+        exact
+        path="/shkzafar/audio/qurantafseer"
+        component={qurantafseer}
+      />
+      <Route exact path="/shkzafar/audio/aam" component={aam} />
+      <Route exact path="/shkzafar/audio/arkan" component={arkan} />
+      <Route exact path="/shkzafar/audio/taweel" component={taweel} />
+      <Route exact path="/shkzafar/audio/muktasar" component={muktasar} />
+      <Route exact path="/shkzafar/audio/khutbah" component={khutbah} />
+      <Route exact path="/shkzafar/books" component={books} />
+      <Route exact path="/shkzafar/about" component={about} />
+    </Switch>
+  </BrowserRouter>
 );
 
 export default Main;
